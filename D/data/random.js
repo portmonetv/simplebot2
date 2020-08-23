565b6c3595a23f300f01822a7b2bc22a7238c491
portmonetv@gmail.com <53911082+portmonetv@users.noreply.github.com>
2020-08-23 08:49:11 +0300

    Update random.js

--- D/data/random.js
+++ D/data/random.js
@@ -45,4 +45,4 @@ function alertRndText(data) {
   refs.random.textContent = randomText;
 }
 
-load_text("http://port-mone.tv/wp-content/uploads/special/1_101/D/data/text.txt");
+load_text("https://portmonetv.github.io/simplebot2/D/data/text.txt");
